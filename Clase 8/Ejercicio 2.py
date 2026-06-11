76399d5ec1fab1548c6b28c69aedfb22e6d79bcd
jose-giron-27 <jose.giron@ufm.edu>
2026-06-11 17:01:47 -0600

    Agrego bonus de precio más caro

--- Clase 8/Ejercicio 2.py
+++ Clase 8/Ejercicio 2.py
@@ -9,18 +9,24 @@ for i in range(n): ##Se establece la condicion
     producto = float(input(f"Cual es el precio del producto {i+1}? : ")) ##Se pide el precio de productos
     total = total + producto
 
+    if producto > precio_caro: ##Bonus de ejercicio
+        precio_caro = producto
+
 if total >= 500: ##Condicion para total con descuento
     descuento = total*0.1
     Total_descuento = total-descuento
     print(f"El total de la compra es: Q{round(total,2)}" )
     print(f"Descuento aplicado: Q{round(descuento,2)}")
     print(f"Total a pagar: Q{round(Total_descuento,2)}")
+        
 else: ##Se establece la condicion sin descuento
     print(f"El total de la compra es: Q{round(total,2)}" )
     print(f"Total a pagar: Q{round(total,2)}")
 
-if producto > precio_caro:
-    precio_caro == producto
+print(f"El precio más caro es: {precio_caro}")
+
+    
+
 
 
 
